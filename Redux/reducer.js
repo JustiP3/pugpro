858f6d6317e444c23f7df7157b1840eacabcbc31
justin <justinphil02@yahoo.com>
2021-01-26 13:57:58 -0800

    change reducer to switch / case statement

--- Redux/reducer.js
+++ Redux/reducer.js
@@ -1,45 +1,50 @@
-function reducer(state, action) {
-    if (action.type === 'LOGIN') {
-        return ({
-            ...state, 
-            loggedIn: true,
-            token: "4ADB8",
-            userData: {
-                userName: action.data.userName
-            },
-            screen: 'landing'
-        })
-    } else if (action.type === 'SIGNUP') {
-        return ({
-            ...state, 
-            loggedIn: true,
-            token: "4ADB8",
-            userData: {
-                userName: action.data.userName
-            },
-            screen: 'configureUser'
-        })
-    } else if (action.type === 'LOGOUT') {
-        return ({
-            ...state, 
-            loggedIn: false,
-            token: null,
-            userData: {},
-            screen: 'home'
-        })
-    } else if (action.type === 'CHANGE_SCREEN') {
-        const newScreen = action.data.screen; 
-        if (state.loggedIn === true) {
-            return ({...state, screen: newScreen})
-        } else {
-            if (newScreen === '' || newScreen === 'signup') {
-                return ({...state, screen: newScreen})
-            } else {
-                return ({...state, screen: 'home'})
-            }
-        }
-    }  else if (action.type === 'SET_USERDATA') {
-        // need to add userdata to state 
+function reducer(state = {loggedin: false, token: null, screen: 'home', userData: {}}, action) {
+
+    switch (action.type) {
+        case 'LOGIN':
+            return ({
+                ...state, 
+                loggedIn: true,
+                token: "4ADB8",
+                userData: {
+                    userName: action.data.userName
+                },
+                screen: 'landing'
+            })
+        case 'SIGNUP':
+            return ({
+                ...state, 
+                loggedIn: true,
+                token: "4ADB8",
+                userData: {
+                    userName: action.data.userName
+                },
+                screen: 'configureUser'
+            })
+        case 'LOGOUT':
+                return ({
+                    ...state, 
+                    loggedIn: false,
+                    token: null,
+                    userData: {},
+                    screen: 'home'
+                })
+        case 'CHANGE_SCREEN':
+                    const newScreen = action.data.screen; 
+                    if (state.loggedIn === true) {
+                        return ({...state, screen: newScreen})
+                    } else {
+                        if (newScreen === 'login' || newScreen === 'signup') {
+                            return ({...state, screen: newScreen})
+                        } else {
+                            return ({...state, screen: 'home'})
+                        }
+                    }
+        case 'SET_USERDATA':
+                //need to set user data
+                break;
+        default:
+                return state 
     } 
 }
 
