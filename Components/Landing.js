69a512fe91157808ddf5fbbc5bad8f546e2fabf8
justin <justinphil02@yahoo.com>
2021-02-01 18:04:38 -0800

    add style to landing component

--- Components/Landing.js
+++ Components/Landing.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect} from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux'
 import LandingHeader from './LandingHeader'
 import ConfigureUser from './ConfigureUser'
@@ -30,34 +30,52 @@ export default function Landing() {
   switch (screen) {
     case 'landing': 
       return (
-        <View >  
+        <View style={styles.container} >  
             <LandingHeader />    
-            <Text>You are Logged In!</Text>
-            <Button title="Logout" onPress={handleLogout}>Log Out</Button>
-          <StatusBar style="auto" />
+            <View style={styles.container}>
+              <Text style={styles.text}>You are Logged In!</Text>
+              <Button title="Logout" onPress={handleLogout}>Log Out</Button>
+        
+          </View>
         </View>
       );
     case 'configureUser':
       return (
-        <View >  
-            <LandingHeader />    
+        <View style={styles.container} >  
+            <LandingHeader /> 
+            <View style={styles.container}>  
             <ConfigureUser handleUserConfig={handleUserConfig} />
             <Button title="Logout" onPress={handleLogout}>Log Out</Button>
-          <StatusBar style="auto" />
+          </View>
         </View>
       );
     default:
       return (
-        <View >  
-            <LandingHeader />    
+        <View style={styles.container} >  
+            <LandingHeader /> 
+            <View style={styles.container}> 
             <Text>Redux state screen does not match a case in Landing component</Text>
             <Button title="Logout" onPress={handleLogout}>Log Out</Button>
-          <StatusBar style="auto" />
+            </View> 
         </View>
       );
   }
  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'dodgerblue',  
+    justifyContent: 'center'   
+  }, 
+  text: {
+    textAlign: 'center'
+  },
+  title: {
+    justifyContent: 'flex-start'
+  }
+});
 
 
 
