9ed65b37db4e6971798006721756473fafde3b9a
justin <justinphil02@yahoo.com>
2021-02-08 16:52:24 -0800

    re-add 'ConfigureUser' component to 'editProfile' screen to allow a user to add other activities of interest

--- Components/Landing.js
+++ Components/Landing.js
@@ -61,6 +61,7 @@ export default function Landing() {
             <LandingHeader /> 
               <View style={styles.container}>  
               <EditProfile /> 
+              <ConfigureUser handleUserConfig={handleUserConfig} />
             </View>
           </View>
         );
